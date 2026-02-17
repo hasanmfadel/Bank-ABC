51ead9d4158f2012ba6b729beadfbe0daa60e8bb
Hasan Hasan <hasan.hasan@Bank-ABC.com>
2026-02-17 15:37:48 +0300

    UPDATED Batch file

--- T24/Migration/BH/Mocks/Mock1/Scripts/Accounts/T24_AAA.sql
+++ T24/Migration/BH/Mocks/Mock1/Scripts/Accounts/T24_AAA.sql
@@ -1,17 +1,3 @@
-<<<<<<< Updated upstream
-
---/* LOG */ INSERT INTO dbo.MIG_LOG (Application,Step,Sub_Step,Event,DateStamp) Values ('ACCOUNTS','Accounts Transformation','Delete Account IDs from Table T24_ACCOUNT_ID','S',getdate());
-DELETE FROM [REPORT].[XBALC_SCF_170225] WHERE XRDN = $(DATE_XRDN);
---/* LOG */ INSERT INTO dbo.MIG_LOG (Application,Step,Sub_Step,Event,DateStamp) Values ('ACCOUNTS','Accounts Transformation','Delete Account IDs from Table T24_ACCOUNT_ID','F',getdate());
-
------------------------------------------
--- Insert Branch 91 accounts
------------------------------------------
---/* LOG */ INSERT INTO dbo.MIG_LOG (Application,Step,Sub_Step,Event,DateStamp) Values ('ACCOUNTS','Accounts Transformation','Insert Urbis Accounts into Table T24_ACCOUNT_ID for Conv.','S',getdate());
-
-INSERT INTO [REPORT].[XBALC_SCF_170225]
-SELECT * FROM URBIS.XBALC WHERE XRDN = $(DATE_XRDN) AND C_TYPE = 'LADL'  AND CONTRACTNO LIKE '24%' AND CNT_STATUS = 'A'
-=======
 :setvar SqlFile T24_AAA.sql
 
 DECLARE @rows BIGINT;
@@ -34,4 +20,3 @@ SELECT * FROM URBIS.XBALC WHERE XRDN = $(DATE_XRDN) AND C_TYPE = ''LADL''  AND C
      ',
      @SqlFile      = N'$(SqlFile)',
      @RowsAffected = @rows OUTPUT;
->>>>>>> Stashed changes
